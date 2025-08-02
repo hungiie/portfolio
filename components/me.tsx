2b54e43ed432eb363833b914da9e22ba9637fa27
Duy hung Tran <duy_hung_tran@sfu.ca>
2025-08-02 15:11:32 -0700

    Fix about page

--- components/me.tsx
+++ components/me.tsx
@@ -2,9 +2,9 @@ import Image from "next/image"
 
 export default function Me() {
     return (
-        <div className="relative group">
-            <div className="transition-all duration-800 rounded-full overflow-hidden w-[120px] h-[120px] md:w-[200px] md:h-[200px] group-hover:scale-[1.07]">
-                <Image src="/abt.jpeg" alt="hungie tran" className="object-cover rounded-full" fill />
+        <div className="relative group md:-mt-15 md:-mr-20">
+            <div className="transition-all duration-800 rounded-xl overflow-hidden w-[120px] h-[120px] md:w-[160px] md:h-[160px] group-hover:scale-[1.07]">
+                <Image src="/abt.jpeg" alt="hungie tran" className="object-cover rounded-3xl" fill />
             </div>
             <p className="
             absolute 
@@ -15,12 +15,14 @@ export default function Me() {
             font-black 
             tracking-tight 
             -z-10 
-            text-4xl
+            text-3xl
+            -top-5 
+            -left-6
             md:text-5xl 
-            -top-6 
-            -left-2 
-            group-hover:-top-9 
-            group-hover:left-1
+            md:-top-8 
+            md:-left-9 
+            group-hover:-top-11 
+            group-hover:-left-6
             ">
                 &lt;/&gt;
             </p>
@@ -32,11 +34,12 @@ export default function Me() {
             text-[var(--colour-yellow)] 
             font-extrabold 
             tracking-tight 
-            -z-10 
+            z-10 
             text-4xl
             md:text-6xl 
             top-2
-            -right-6 
+            -right-6
+            md:top-2
             md:-right-11 
             group-hover:top-8 
             group-hover:-right-10
@@ -52,13 +55,13 @@ export default function Me() {
             font-medium 
             tracking-tight 
             text-8xl
-            -bottom-13
-            right-0
-            md:text-[10rem] 
-            md:-bottom-21
-            md:right-2 
-            group-hover:-bottom-24 
-            group-hover:right-6
+            -bottom-15
+            right-3
+            md:text-[8rem] 
+            md:-bottom-20
+            md:right-6 
+            group-hover:-bottom-22 
+            group-hover:right-9
             ">
                 *
             </p>
