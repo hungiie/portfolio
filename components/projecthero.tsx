21d2e4679f5fe26740c81e924ea3acc6b19b7fb0
Duy hung Tran <duy_hung_tran@sfu.ca>
2025-08-23 10:03:46 -0700

    Fixed tracking problem

--- components/projecthero.tsx
+++ components/projecthero.tsx
@@ -18,7 +18,7 @@ export default function ProjectHero(props: Props) {
             <p className="px-6 py-3 text-md md:text-md text-white rounded-full inline-block" style={{ backgroundColor: props.colourCode }}>
                 {props.name}
             </p>
-            <p className="text-[var(--colour-bodytext)] text-4xl md:text-7xl font-semibold mt-10 mb-15 tracking-[-0.15rem] leading-[120%] md:leading-[110%] w-full md:w-5/6">{props.description}</p>
+            <p className="text-[var(--colour-bodytext)] text-4xl md:text-7xl font-semibold mt-10 mb-15 tracking-tight md:tracking-[-0.15rem] leading-[120%] md:leading-[110%] w-full md:w-5/6">{props.description}</p>
             <div className="flex items-center">
                 <div className="mr-10 md:mr-20">
                     <Button variant="blue-hyperlink" link={props.url1} text={props.linkText1} target="_self" textSize="text-md"/>
