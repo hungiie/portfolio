49103af3ebdf1dd2bb585ad5349e1aaae5de1ac0
Hung Tran <duyhungtran@Hungies-MacBook-Pro-1998.local>
2025-12-28 13:11:28 -0800

    Update page.tsx

--- app/rules/page.tsx
+++ app/rules/page.tsx
@@ -68,7 +68,7 @@ export default function Rules() {
                 <p className="mb-1 text-sm text-[var(--colour-bodytext)] leading-loose">Special thanks to:</p>
                 <p className="mb-6 text-sm text-[var(--colour-bodytext)] leading-loose">Tian Jia (Senior UX Designer & UX Lead) and Hosoo Lee (Product Manager)</p>
 
-                <div className="flex flex-col justify-center bg-[var(--colour-grey)] px-7 py-7 ounded-2xl">
+                <div className="flex flex-col justify-center bg-[var(--colour-grey)] px-7 py-7 rounded-2xl">
                     <p className="text-sm text-[var(--colour-bodytext)] leading-loose">Due to NDA restrictions, I&rsquo;m unable to share project details publicly, but I&rsquo;d be happy to elaborate on my experience and design contributions during an interview.</p>
                     <p className="text-sm text-[var(--colour-bodytext)] mt-5 leading-loose">Feel free to send me an <span className="inline-block"><Button link="mailto:d.hungtran12@gmail.com" target="_blank" variant="blue-hyperlink" text="email" textSize="text-sm"/></span>!</p>
                 </div>
