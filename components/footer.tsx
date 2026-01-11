e2d3142aa753f2722432af663acf6338a4977642
Hung Tran <tranndhungg@gmail.com>
2026-01-10 22:43:32 -0800

    tate mcrae

--- components/footer.tsx
+++ components/footer.tsx
@@ -3,13 +3,15 @@ import Button from "./button";
 export default function Footer() {
     return (
         <div className="mt-40 pb-28 flex flex-col items-center">
-            <div className="text-center">
-                <p className="mb-1 text-sm text-[var(--colour-bodytext-3)]">Designed and coded by me using Tailwind CSS and Next.js</p>
-                <p className="mb-7 text-sm text-[var(--colour-bodytext-3)]">© 2025 Hung</p>
+            <div className="text-center w-full md:w-5/6">
+                <p className="mb-1 text-sm text-[var(--colour-bodytext-3)] leading-loose">This site was designed and coded by me with Figma, Tailwind CSS, Next.js, and a dream.</p>
+                {/* <p className="text-sm text-[var(--colour-bodytext-3)]">© 2025 Hung</p> */}
             </div>
-            <div className="flex w-full md:w-4/5 lg:w-4/5 justify-between">
+            <div className="mb-7"></div>
+            <div className="flex w-full md:w-4/5 justify-between">
                 <Button variant="blue-hyperlink" text="LinkedIn" link="https://www.linkedin.com/in/hung-tran-profile" target="_blank" textSize="text-sm"></Button>
                 <Button variant="blue-hyperlink" text="d.hungtran12@gmail.com" link="mailto:d.hungtran12@gmail.com" target="_blank" textSize="text-sm"></Button>
+                {/* <Button variant="blue-hyperlink" text="GitHub" link="https://github.com/hungiie" target="_blank" textSize="text-sm"></Button> */}
                 <Button variant="blue-hyperlink" text="Resume" link="https://drive.google.com/file/d/1F2UXNXNltAkH-Y0qinUaZDLaMNDMd6Mh/view" target="_blank" textSize="text-sm"></Button>
             </div>
         </div>
