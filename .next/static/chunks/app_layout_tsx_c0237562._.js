777cdcbdbb0eee1af5c16a077533e35e8ed6e720
Hung Tran <tranndhungg@gmail.com>
2026-01-12 16:53:15 -0800

    Stop tracking Next.js build output

--- .next/static/chunks/app_layout_tsx_c0237562._.js
+++
@@ -1,9 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/[root-of-the-server]__c687b685._.css",
-  "static/chunks/node_modules_b3f0e415._.js",
-  "static/chunks/components_themeprovider_tsx_6d447839._.js"
-],
-    source: "dynamic"
-});
