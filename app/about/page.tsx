6ba95871b7480ed22047b354b60b36c590ad8e83
Hung Tran <tranndhungg@gmail.com>
2026-02-01 11:53:55 -0800

    Update page.tsx

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -15,8 +15,13 @@ export default function About() {
         <MyNav2 variant="Abt to home"/>
         <div className="w-full px-10 md:w-2/3 md:px-0 lg:w-1/3 2xl:w-1/4 mt-24">
             <SlideUpNoThres>
-                <div className="mb-9">
+                <div className="mb-9 flex justify-between">
                     <MyButton variant="blue-hyperlink" text="Return to home" link="/" textSize="text-sm" target="_self"/>
+                    <div className="flex">
+                        {/* <MyButton variant="blue-button-outline" text="About me" link="about" textSize="text-sm" target="_self"/>
+                        <div className="ml-3"></div> */}
+                        <MyButton variant="blue-button" text="Resume" link="https://drive.google.com/file/d/1F2UXNXNltAkH-Y0qinUaZDLaMNDMd6Mh/view" textSize="text-sm" target="_blank"/>
+                    </div>
                 </div>
 
                 <div className="w-full flex">
