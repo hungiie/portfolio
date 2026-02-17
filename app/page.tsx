e98f7120ac13ad2471bd53a26258f4d372c21ad1
Hung Tran <tranndhungg@gmail.com>
2026-02-17 15:20:46 -0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -78,7 +78,7 @@ export default function Home2() {
                   <ProjectThumbnail2 isLink="image-link-hover" link="organizations" name="Samsung Knox Manage: Organizations" desc="Designed a device management feature that organizes devices into nested groups and enables hierarchical device management." imageSrc="/org.png" imageSrcDark="/org-hover.png" target="_self"/>
               </SlideUp>
               <SlideUp>
-                  <ProjectThumbnail2 isLink="image-link-hover" link="https://drive.google.com/file/d/1d0jGR0QJN3mzwX6o9n0C54g6aHej0I9l/view" name="Memory Mate" desc="React Native note-taking app for senior citizens, integrating Apple Maps API, Text-to-speech API, and Firebase for backend database and user authentication." imageSrc="/memorymate.png" imageSrcDark="/memorymate-hover.png" target="_blank"/>
+                  <ProjectThumbnail2 isLink="image-link" link="https://drive.google.com/file/d/1d0jGR0QJN3mzwX6o9n0C54g6aHej0I9l/view" name="Memory Mate" desc="React Native note-taking app for senior citizens, integrating Apple Maps API, Text-to-speech API, and Firebase for backend database and user authentication." imageSrc="/memorymate.png" imageSrcDark="/memorymate-hover.png" target="_blank"/>
               </SlideUp>
               {/* <SlideUp>
                   <ProjectThumbnail2 isLink="image-link" link="https://drive.google.com/file/d/1d0jGR0QJN3mzwX6o9n0C54g6aHej0I9l/view" name="Jackalope" desc="Social event and community finder app, helping international students settle into a new country, with AI-powered suggestions and built-in feedback mechanisms." imageSrc="/jackalopenew.png" imageSrcDark="/memorymate.png" target="_blank"/>
