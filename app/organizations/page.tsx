1127e73781d605470874b0442426c25566ac8188
Hung Tran <tranndhungg@gmail.com>
2026-02-21 00:45:26 -0800

    sodsokdq[

--- app/organizations/page.tsx
+++ app/organizations/page.tsx
@@ -107,9 +107,9 @@ export default function Organizations() {
                             <div className="flex flex-col justify-center bg-[#fbfbfb] p-10 rounded-2xl">
                                 <p className="text-sm text-[var(--main-blue)] leading-loose">Problem ⚠️</p>
                                 <div className="mb-9"></div>
-                                <p className="text-lg text-[var(--colour-bodytext)] leading-loose">Due to limited development bandwidth, the <span className="font-semibold font-[var(--colour-pure)] underline">inheritance logic among parent-to-child entities</span><span className="text-[var(--main-blue)]">(*)</span> needs to be visualized with minimal dev effort - without adding new patterns, animations, or complex interactions.</p>
+                                <p className="text-lg text-[var(--colour-bodytext)] leading-loose">Due to limited development bandwidth, the <span className="font-semibold font-[var(--colour-pure)] underline">inheritance logic among parent-to-child entities</span> needs to be visualized with minimal dev effort - without adding new patterns, animations, or complex interactions.</p>
                                 <div className="mb-9"></div>
-                                <p className="text-sm text-[var(--colour-bodytext)] leading-loose"><span className="text-[var(--main-blue)]">(*) </span>When a setting is assigned to a parent organization, it is inherited by all of its child organizations. This setting is not editable in the child organization, and can only be overriden by assigning a new setting to the child organization.</p>
+                                <p className="text-sm text-[var(--colour-bodytext)] leading-loose">When a setting is assigned to a parent organization, it is inherited by all of its child organizations. This setting is not editable in the child organization, and can only be overriden by assigning a new setting to the child organization.</p>
                             </div> 
                             <div className="mb-4"></div>
                             <div className="flex flex-col justify-center bg-[#fbfbfb] p-10 rounded-2xl">
