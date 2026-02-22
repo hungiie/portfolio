74f16e5a3ed50687f8a5669c269444938c4dac5f
Hung Tran <tranndhungg@gmail.com>
2026-02-21 20:56:46 -0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -91,9 +91,9 @@ export default function Home2() {
               <SlideUp>
                   <ProjectThumbnail2 isLink="image-link" link="https://drive.google.com/file/d/1d0jGR0QJN3mzwX6o9n0C54g6aHej0I9l/view" name="Memory Mate" desc="React Native note-taking app for senior citizens, integrating Apple Maps API, Text-to-speech API, and Firebase for backend database and user authentication." imageSrc="/memorymate.png" imageSrcDark="/memorymate-hover.png" target="_blank"/>
               </SlideUp>
-              {/* <SlideUp>
-                  <ProjectThumbnail2 isLink="image-link" link="visuals" name="Visual design" desc="I like to play around with graphic design, motion, and visual design. Here's the damage." imageSrc="/visuall.png" imageSrcDark="/memorymate-hover.png" target="_self"/>
-              </SlideUp> */}
+              <SlideUp>
+                  <ProjectThumbnail2 isLink="image-link" link="visuals" name="Visual design" desc="I like to play around with visual design and motions. Here's the damage." imageSrc="/visuall.png" imageSrcDark="/memorymate-hover.png" target="_self"/>
+              </SlideUp>
               {/* <SlideUp>
                   <ProjectThumbnail2 isLink="image-link" link="https://drive.google.com/file/d/1d0jGR0QJN3mzwX6o9n0C54g6aHej0I9l/view" name="Jackalope" desc="Social event and community finder app, helping international students settle into a new country, with AI-powered suggestions and built-in feedback mechanisms." imageSrc="/jackalopenew.png" imageSrcDark="/memorymate.png" target="_blank"/>
               </SlideUp> */}
