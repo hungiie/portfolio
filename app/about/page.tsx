3e7ede72f80e976f5e069956392f354138443795
Hung Tran <tranndhungg@gmail.com>
2026-04-02 19:22:05 -0700

    Update page.tsx

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -89,7 +89,7 @@ export default function About() {
                 <div className="mb-30"></div>
 
                 <div className="w-full flex items-center justify-center">
-                    <div className="w-full h-auto relative mb-12">
+                    <div className="w-full h-auto relative mb-11">
                         <Image src="/small.jpg" width={2040} height={1179} alt="hung tran" className="object-cover rounded-xl shadow-md mb-3" style={{ filter: 'contrast(80%)' }}/>
                         <p className="text-sm text-[var(--colour-bodytext-3)] leading-loose">Me but #2008 ✨</p>
                         {/* <Polaroid imageSrc="/small.jpg" alt="hung tran" desc="Summer 2008 🐦" angle={-4} textSize="text-xl"/> */}
